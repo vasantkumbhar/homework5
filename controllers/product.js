c4ef8a0cb2ebc3dfd62f2623134f0246c962fa01
Vasant <VKumbhar2@slb.com>
2019-08-05 17:47:42 +0530

    added jwt token verification for all api other than auth

--- controllers/product.js
+++ controllers/product.js
@@ -19,51 +19,78 @@ app.get('/api/products', (req, res) => {
   });
 });
 app.post("/api/products", (req, res) => {
-  if (!req.body.name || req.body.name === "") {
-    return res.status(400).send({
-      success: "false",
-      message: "Name is required"
-    });
-  }
-  const product = {
-    id: db.product.length + 1,
-    name: req.body.name,
-    brand: req.body.brand,
-    price: req.body.price,
-    options: req.body.options,
-    reviews: req.body.reviews
-  };
-  db.product.push(product);
-  res.status(200).send({
-    success: "true",
-    message: "Products Added successfully",
-    data: product
-  });
+  jwt.verify(req.headers.authorization, 'secret', (err) => {
+    if(err){
+      //If error send Forbidden (403)
+        console.log('ERROR: Could not connect to the protected route');
+        res.sendStatus(403);
+    } else {
+      //If token is successfully verified, we can send the autorized data 
+      if (!req.body.name || req.body.name === "") {
+        return res.status(400).send({
+          success: "false",
+          message: "Name is required"
+        });
+      }
+      const product = {
+        id: db.product.length + 1,
+        name: req.body.name,
+        brand: req.body.brand,
+        price: req.body.price,
+        options: req.body.options,
+        reviews: req.body.reviews
+      };
+      db.product.push(product);
+      res.status(200).send({
+        success: "true",
+        message: "Products Added successfully",
+        data: product
+      });
+    }
+  })
 });
 
 // get specific products
 app.get("/api/products/:id", (req, res) => {
-  const id = req.params.id;
-  db.product.map(product => {
-    if (product.id === id) {
-      res.status(200).send({
-        success: "true",
-        message: "Product retrieved successfully",
-        data: product
+  jwt.verify(req.headers.authorization, 'secret', (err) => {
+    if(err){
+      //If error send Forbidden (403)
+        console.log('ERROR: Could not connect to the protected route');
+        res.sendStatus(403);
+    } else {
+      //If token is successfully verified, we can send the autorized data 
+      const id = req.params.id;
+      db.product.map(product => {
+        if (product.id === id) {
+          res.status(200).send({
+            success: "true",
+            message: "Product retrieved successfully",
+            data: product
+          });
+        }
       });
     }
-  });
+  })
 });
 
 app.get("/api/products/:id/reviews", (req, res) => {
-  const id = req.params.id;
-  db.product.map(product => {
-    if (product.id === id) {
-      res.status(200).send({
-        success: "true",
-        message: "Product review retrieved successfully",
-        data: product.reviews
+  jwt.verify(req.headers.authorization, 'secret', (err) => {
+    if(err){
+      //If error send Forbidden (403)
+        console.log('ERROR: Could not connect to the protected route');
+        res.sendStatus(403);
+    } else {
+      //If token is successfully verified, we can send the autorized data 
+      const id = req.params.id;
+      db.product.map(product => {
+        if (product.id === id) {
+          res.status(200).send({
+            success: "true",
+            message: "Product review retrieved successfully",
+            data: product.reviews
+          });
+        }
       });
     }
-  });
+  })
 });
--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,20 @@
 var appInstance = require("../app");
-var {app,db} = appInstance.appConst;
+var { app, db, jwt } = appInstance.appConst;
 
 // get all users
-app.get('/api/users', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'Users retrieved successfully',
+app.get("/api/users", (req, res) => {
+  jwt.verify(req.headers.authorization, "secret", err => {
+    if (err) {
+      //If error send Forbidden (403)
+      console.log("ERROR: Could not connect to the protected route");
+      res.sendStatus(403);
+    } else {
+      //If token is successfully verified, we can send the autorized data
+      res.status(200).send({
+        success: "true",
+        message: "Users retrieved successfully",
         data: db.users
-    })
+      });
+    }
+  })
 });
